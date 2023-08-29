11a0dc8bcf08e79d9b8f20f33e593bcea1c40459
Amrinder Rai <amrinderrai94@gmail.com>
2023-08-29 16:41:20 +0100

    Add control panel

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,23 @@
 import "./App.css";
 import "leaflet/dist/leaflet.css"
 import { MapContainer, TileLayer } from 'react-leaflet'
+import ControlPanel from './ControlPanel.tsx'
+
 
 
 function App() {
 
   return (
+    <div>
     <MapContainer center={[51.505, -0.09]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'.png"
       />
     </MapContainer>
+    <ControlPanel /> 
+    </div>
+    // Sidebar component here??? 
   )
 }
 
---
+++ react-app/src/ControlPanel.tsx
@@ -0,0 +1,35 @@
+import "./ControlPanel.css";
+
+function ControlPanel() {
+    return (
+        <div className='control-panel'>
+            <div className="severity-control">
+                Slight: <input type="checkbox" id="slightCheck" /> <br/>
+                Serious: <input type="checkbox" id="seriousCheck" /> <br/>
+                Fatal: <input type="checkbox" id="fatalCheck" /> <br/>
+            </div>
+
+            <div className="vehicle-type-control">
+                All Vehicles: <input type="checkbox" id="allvehiclesCheck" /> <br/> <br/>
+                Cycles: <input type="checkbox" id="cyclesCheck" />     
+                Two-wheeled motor vehicles: <input type="checkbox" id="2wheeledCheck" />    
+                Cars: <input type="checkbox" id="carsCheck" />
+                Buses/Coaches: <input type="checkbox" id="BusesCheck" />
+                LGVs: <input type="checkbox" id="lgvsCheck" />
+                HGVs: <input type="checkbox" id="hgvsCheck" />
+            </div>
+
+            <div className="year-control">
+                <input type="range" min="2005" max="2022" id="slider" />
+                <h1><text id="currentYear">2010</text></h1>
+            </div>
+
+        </div>
+    )
+
+    // TO DO: Column styling for div components 
+
+    
+}
+
+export default ControlPanel 
