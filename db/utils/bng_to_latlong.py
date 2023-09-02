eb3269155e064684330d54491130409dfd5e2dd7
Amrinder Rai <amrinderrai94@gmail.com>
2023-09-02 12:40:44 +0100

    bng to latlong conversion util function

--- db/utils/bng_to_latlong.py
+++ db/utils/bng_to_latlong.py
@@ -1,1 +1,13 @@
-# TO DO: script/method to convert BNG (Easting & Northing) co-ordinates into Latitude & Longitude 
+from pyproj import Proj, transform
+import pandas as pd 
+
+v84 = Proj(proj="latlong",towgs84="0,0,0",ellps="WGS84")
+v36 = Proj(proj="latlong", k=0.9996012717, ellps="airy",
+        towgs84="446.448,-125.157,542.060,0.1502,0.2470,0.8421,-20.4894")
+vgrid = Proj(init="world:bng")
+
+
+def BNGtoLatLong(easting, northing):
+    """Returns [longitude, latitude] as Pandas series"""
+    vlon36, vlat36 = vgrid(easting, northing, inverse=True)
+    return pd.Series(transform(v36, v84, vlon36, vlat36))
